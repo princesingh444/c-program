42c6420d25d59bb8529383b33fabe9fb70c34d2a
prince singh <princesingh36006457@gmail.com>
2025-09-17 21:49:03 +0530

    firstcommit

---
+++ lect1/hello.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main (){
+    int age=18;
+    char favchar='p';
+    float favnum=4.44;
+	
+	printf(" my name is prince singh \n");
+    printf(" Age = %d\n",age);
+    printf(" Favchar = %c\n",favchar);
+    printf(" Favnum = %f\n",favnum);
+}
