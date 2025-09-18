0497b97a733ced1618440340d3b6de824754bdbe
prince singh <princesingh36006457@gmail.com>
2025-09-18 08:56:50 +0530

    firstcommit

---
+++ lect4/hello.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main(){
+
+    // int num=2456;
+    // int sum=0;
+    // while (num>0)
+    // { int lastDigit=num%10;
+    //    sum=sum+lastDigit;
+    //    num=num/10;
+          
+    // }
+    // printf("%d",sum);
+    
+
+    //  int i=0;
+    //  while (i<10)
+    //  {
+    //     printf("hello\n");
+    //     i++;
+    //  }
+    
+
+     for(int i =1;i<=5;i++){
+         // printf("hello\n");
+         printf("%d\n",i);
+     }
+    // for(int i =10;i>0;i--){
+    //     // printf("%d\n",i);
+    //     if(i%2==1){
+    //          printf("%d\n",i);
+    //     }
+    // }
+    //  int sum=0;
+    // for(int i =1;i<=5;i++){
+    //     sum=sum+i;
+    //     // sum+=i;
+
+      
+    // }
+    //   printf("%d\n",sum);
+
+
+}
