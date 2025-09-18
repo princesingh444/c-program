f7988cb0a6f5e30f264e8837a223e7eab303402c
prince singh <princesingh36006457@gmail.com>
2025-09-18 08:47:00 +0530

    firstcommit

---
+++ lect3/hello.c
@@ -0,0 +1,33 @@
+    #include <stdio.h>
+    int main(){
+        int a ;
+        printf("Enetr the number = ");
+        scanf("%d",&a);
+        switch (a)
+        {
+        case 1:
+            printf("monday");
+            break;
+        case 2:
+            printf("tuesday");
+        case 3:
+        printf("wednesday");
+            break;
+           case 4:
+        printf("thusday");
+            break;
+           case 5:
+        printf("friday");
+            break;
+           case 6:
+        printf("saturday");
+            break;
+           case 7:
+        printf("sunday");
+            break;
+           default:
+        printf(" please enter vailid number");
+            break;
+        }
+    return 0;
+    }
